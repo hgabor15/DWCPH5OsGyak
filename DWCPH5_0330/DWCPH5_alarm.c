fc18934cd58db1116093ffd07ec54447c9f53bc2
hgabor15 <hegedus.gabor91@protonmail.com>
2021-03-30 15:36:01 +0200

    gyak8

---
+++ DWCPH5_0330/DWCPH5_alarm.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+
+void sig_handler(int signum){
+
+  printf("Inside handler function\n");
+}
+
+int main(){
+
+  signal(SIGALRM,sig_handler);
+
+  alarm(2);
+
+  for(int i=1;;i++){
+
+    printf("%d : Inside main function\n",i);
+    sleep(1);
+}
+return 0;
+}
